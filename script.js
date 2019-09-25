af362180ca44e8c2c3298a47a6a460b9a7d9f039
sharoduwi <55786655+sharoduwi@users.noreply.github.com>
2019-09-25 16:52:14 +0300

    upd source code #4

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ $(.'carousel').carousel({
     interval: 300,
     wrap:false
 })
+.btn btn-info{
+    background: navy; /* Синий цвет фона */ 
+    color: white; /* Белые буквы */ 
+    font-size: 9pt;
+    
+}
